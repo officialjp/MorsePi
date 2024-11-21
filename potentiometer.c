70edfd8b862a5f7e045420d4f4cb766329cf1efc
Birch <ab04138@heron018.eps.surrey.ac.uk>
2024-11-21 15:53:34 +0000

    Added comments

--- potentiometer.c
+++ potentiometer.c
@@ -7,16 +7,19 @@
 #define POTENTIOMETER_MIN       20
 #define POTENTIOMETER_MAX       4050
 
+//initialises the potentiometer
 void potentiometer_init() {
     adc_init();
     adc_gpio_init(POTENTIOMETER_PIN);
 }
 
+//reads the raw input given by the potentiometer
 unsigned int potentiometer_read_raw() {
     adc_select_input(POTENTIOMETER_PIN - 26);
     return adc_read();
 }
 
+//maps the input to a certain structure
 int map(
     int value,
     int original_min,
@@ -30,12 +33,14 @@ int map(
     return (value - original_min) * range / original_range + min;
 }
 
+//clumps the input
 int clamp(int value, int min, int max) {
     if (value < min) return min;
     if (value > max) return max;
     return value;
 }
 
+//reads the potentiometer with a given limit
 unsigned int potentiometer_read(unsigned int limit) {
     return map(
         clamp(potentiometer_read_raw(), POTENTIOMETER_MIN, POTENTIOMETER_MAX),
